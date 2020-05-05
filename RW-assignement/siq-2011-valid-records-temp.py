57c8724d6c60bca3141b7803c2f193da2c8af00c
Sidra Iqbal <siqbal8@hawk.iit.edu>
2020-05-05 01:19:52 -0500

    valid records temp

---
+++ RW-assignement/siq-2011-valid-records-temp.py
@@ -0,0 +1,19 @@
+from pyspark.sql.functions import to_date
+from pyspark.sql.types import IntegerType
+from pyspark.sql.functions import StringType
+from pyspark.sql import Row
+from pyspark.sql.types import StructField, StructType, FloatType
+from pyspark.sql import SparkSession
+
+
+spark = SparkSession.builder.appName("Demo Spark Python Cluster Program").getOrCreate()
+
+df = spark.read.format("parquet").load("hdfs://namenode/output/itmd-521/siq/2011/parquet-file")
+
+
+# filtering out Air_Temperature
+df_valid_temp=df.filter(df.Air_Temperature!= 999.9)
+
+df_valid_temp.count() #125266430 
+
+df_valid_temp.write.format("parquet").mode("overwrite").save("hdfs://namenode/output/itmd-521/siq/2011/valid-records-temp")
